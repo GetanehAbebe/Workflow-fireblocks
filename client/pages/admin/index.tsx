5a0a9423db2ca962b15e544049ec5dfd8af49cd8
GetanehAbebe <82867396+GetanehAbebe@users.noreply.github.com>
2022-09-02 13:47:05 +0300

    Update index.tsx

--- client/pages/admin/index.tsx
+++ client/pages/admin/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import type { NextPage } from "next";
 import Container from "../../components/Container";
 import WorkFlow from "../../components/WorkFlow";
-const Signup: React.FC<{}> = () => {
-  return <WorkFlow isAdmin={true} />;
-};
+
 const Admin: React.FC<{}> = () => {
   return <WorkFlow isAdmin={true} />;
 };
