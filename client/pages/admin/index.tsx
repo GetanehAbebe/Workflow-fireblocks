8f2249c5266b4eebee08159b7bed7120ee5679f9
GetanehAbebe <82867396+GetanehAbebe@users.noreply.github.com>
2022-09-02 13:47:36 +0300

    Update index.tsx

--- client/pages/admin/index.tsx
+++ client/pages/admin/index.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import type { NextPage } from "next";
-import Container from "../../components/Container";
 import WorkFlow from "../../components/WorkFlow";
 
 const Admin: React.FC<{}> = () => {
