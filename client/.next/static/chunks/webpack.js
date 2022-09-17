f4534abaeda400aeb2530fea07f5151655ce1b3f
GetanehAbebe <abebegetaneh.ag@gmail.com>
2022-09-17 21:49:59 +0300

    change script to run only front

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b7b4fed789d770cc"; }
+/******/ 		__webpack_require__.h = function() { return "a65ea96f6bec82aa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/.next/static/webpack/webpack.a27f1debbf597d52.hot-update.js
+++ client/.next/static/webpack/webpack.a27f1debbf597d52.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "bce3e8238f4a7c85"; }
+/******/ 	__webpack_require__.h = function() { return "a65ea96f6bec82aa"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
