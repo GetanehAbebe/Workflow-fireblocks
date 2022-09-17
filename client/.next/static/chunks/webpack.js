948c7800fc39e5a8d8c176a5b4b7943e9bf4dfcf
GetanehAbebe <abebegetaneh.ag@gmail.com>
2022-09-17 20:42:14 +0300

    add start to script

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6fa4b1ce6a71c970"; }
+/******/ 		__webpack_require__.h = function() { return "cc02365dbb2c8460"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
